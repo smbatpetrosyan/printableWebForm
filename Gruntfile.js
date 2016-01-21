9aeae5889b60056dcbc765023eab521f81bd0f9a
Arpine Petrosyan <arpineh_p@instigatemobile.com>
2016-01-21 09:41:34 +0400

    Removed unnecessary files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
         uglify: {
             compress: {
                 files: {
-                    'grunt/compress.js': ['js/*.js']
+                    'build/compress.js': ['js/*.js']
                 }
             }
         },
@@ -14,29 +14,32 @@ module.exports = function(grunt) {
             absolute: {
                 files: [{
                     src: ['css/*.css'],
-                    dest: 'grunt/absolute.css'
+                    dest: 'build/absolute.css'
                 }]
             }
         },
         includes: {
             includePath: {
-                src: ['applicationFormGrunt.html'],
-                dest: 'grunt/printableWebForm.html',
+                src: ['main.html'],
+                dest: 'build/printableWebForm.html',
                 options: {
-                    includePath: 'grunt/'
+                    includePath: 'build/'
                 }
             }
-        }
+        },
+        clean: ['build/*.css', 'build/*.js']
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-includes');
-    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'includes']);
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'includes', 'clean']);
 };
 /*
 sudo npm install grunt-contrib-jshint --save-dev
 sudo npm install grunt-contrib-uglify --save-dev
 sudo npm install grunt-contrib-cssmin --save-dev
 sudo npm install grunt-includes --save-dev
+sudo npm install grunt-contrib-clean --save-dev
 */
